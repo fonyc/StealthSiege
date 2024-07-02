f863a1b8c7528be62f6100f197371a004f444d4e
fonyc <fonycas@hotmail.com>
2024-07-02 03:21:12 +0200

    player inputs Interact, move, look, sprint done

--- Source/StealthSiege/Private/Character/SS_Character.cpp
+++ Source/StealthSiege/Private/Character/SS_Character.cpp
@@ -4,9 +4,12 @@
 #include "Character/SS_Character.h"
 
 #include "Camera/CameraComponent.h"
-#include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/Controller.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 ASS_Character::ASS_Character()
 {
@@ -27,16 +30,16 @@ ASS_Character::ASS_Character()
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
-	
+
 	// GetCapsuleComponent()->InitCapsuleSize(60.f, 96.0f);
 	GetMesh()->SetRelativeLocation(FVector(0.f, 0.f, -90.f));
-	
+
 	// static ConstructorHelpers::FObjectFinder<USkeletalMesh>SkeletalMeshAsset(TEXT("/Game/PolygonFanMiniChr/Models/Characters/SK_Fantasy_RougeMale_01.SK_Fantasy_RougeMale_01'"));
 	// if (SkeletalMeshAsset.Succeeded())
 	// {
 	// 	GetMesh()->SetSkeletalMesh(SkeletalMeshAsset.Object);
 	// }
-	
+
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
@@ -47,11 +50,71 @@ ASS_Character::ASS_Character()
 void ASS_Character::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//Add mapping context to player
+	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+void ASS_Character::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+	GEngine->AddOnScreenDebugMessage(0, 5.f, FColor::Yellow,FString::Printf(TEXT("MovementVector: %s"), *MovementVector.ToString()));
+	if (Controller != nullptr)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void ASS_Character::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+	GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Green,FString::Printf(TEXT("LookAxisVector: %s"), *LookAxisVector.ToString()));
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void ASS_Character::SprintStart(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(2, 5.f, FColor::Blue,TEXT("SprintStart"));
+	GetCharacterMovement()->MaxWalkSpeed = 3000.f;
+}
+
+void ASS_Character::SprintEnd(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(2, 5.f, FColor::Blue,TEXT("SprintEnd"));
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+}
+
+void ASS_Character::Interact(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(3, 5.f, FColor::Red,TEXT("Interact"));
 }
 
 void ASS_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-}
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASS_Character::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASS_Character::Look);
+		EnhancedInputComponent->BindAction(InteractAction, ETriggerEvent::Started, this, &ASS_Character::Interact);
+		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Started, this, &ASS_Character::SprintStart);
+		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Completed, this, &ASS_Character::SprintEnd);
+	}
+}
--- Source/StealthSiege/Public/Character/SS_Character.h
+++ Source/StealthSiege/Public/Character/SS_Character.h
@@ -8,6 +8,8 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+class UInputComponent;
+class UInputMappingContext;
 
 UCLASS()
 class STEALTHSIEGE_API ASS_Character : public ACharacter
@@ -16,19 +18,42 @@ class STEALTHSIEGE_API ASS_Character : public ACharacter
 
 public:
 	ASS_Character();
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 protected:
+	
 	virtual void BeginPlay() override;
+
+	void Move(const struct FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void SprintStart(const FInputActionValue& Value);
+	void SprintEnd(const FInputActionValue& Value);
+	void Interact(const FInputActionValue& Value);
 	
 private:
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<USpringArmComponent> CameraBoom;
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UCameraComponent> FollowCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta =(AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta =(AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> SprintAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta =(AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> InteractAction;
 
 };
